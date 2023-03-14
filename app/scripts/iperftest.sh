40e8f95925b2f6a90262fe8291ec79bd24b25e77
Phil Reese <preese@gmail.com>
2023-03-14 12:50:39 -0700

    Moved to using iperf3's JSON format

--- app/scripts/iperftest.sh
+++ app/scripts/iperftest.sh
@@ -4,13 +4,16 @@ DB_NAME="${DB_NAME:-iperftest}"
 DB_USERNAME="${DB_USERNAME:-admin}"
 DB_PASSWORD="${DB_PASSWORD:-password}"
 DATE=$(date +%s)
+LOGDATE=$(date)
 IPERFHOST=srn-dm.stanford.edu
 PUBLIC_IP=$(curl ifconfig.me)
 
-echo "running iperftest"
-let "UL = $(iperf3 -4 -O1 -fK -c $IPERFHOST | grep receiver | awk '{print $7 }') * 1000"
-sleep 7
-let "DL = $(iperf3 -4 -O1 -fK -R -c $IPERFHOST | grep receiver | awk '{print $7 }') * 1000"
+echo "running iperftest $LOGDATE"
 
-curl -s -S -XPOST "$DB_HOST/write?db=$DB_NAME&precision=s&u=$DB_USERNAME&p=$DB_PASSWORD" \
-    --data-binary "bandwidth,server=$IPERFHOST,public=$PUBLIC_IP down=$DL,up=$UL $DATE"
+UL="$(iperf3 -4 -J -O1 -t3 -p 5202 -c "$IPERFHOST" | jq -r '.end.sum_received.bits_per_second' | awk -F "." '{print $1}'  )"
+
+sleep 10
+
+DL="$(iperf3 -4 -J -O1 -R -t3 -p 5202 -c "$IPERFHOST" | jq -r '.end.sum_received.bits_per_second' | awk -F "." '{print $  1}')"
+
+curl -s -S -XPOST "$DB_HOST/write?db=$DB_NAME&precision=s&u=$DB_USERNAME&p=$DB_PASSWORD" --data-binary "bandwidth,server=$IPERFHOST,public=$PUBLIC_IP down=$DL,up=$UL $DATE"
--- app/scripts/ping.sh
+++ app/scripts/ping.sh
@@ -7,18 +7,15 @@ DB_PASSWORD="${DB_PASSWORD:-password}"
 DATE=$(date +%s)
 GW=$(ip -4 route | awk '/^default/ { print $3 }')
 PUBLIC_IP=$(curl ifconfig.me)
-COUNT=20
-TIMEOUT=1
+COUNT=3
+TIMEOUT=3
 
-for host in $GW 1.1.1.1 8.8.8.8; do
+for host in $GW 1.1.1.1 8.8.8.8 9.9.9.9 76.76.19.19 185.228.168.9 208.67.222.222; do
 
   result=$(ping -c $COUNT -W $TIMEOUT $host)
-  #min=$(echo $result | awk -F"[,/]" '/round/{print $5}')
-  avg=$(echo $result | awk -F"[,/]" '/round/{print $6}')
-  #max=$(echo $result | awk -F"[,/]" '/round/{print $7}')
+  avg=$(echo $result | grep "= [0-9]" | cut -f4 -d"/" | cut -f3 -d" " )
   loss=$(echo $result | grep -o "[0-9]\+%" | cut -f1 -d% )
 
-  curl -s -S -XPOST "$DB_HOST/write?db=$DB_NAME&precision=s&u=$DB_USERNAME&p=$DB_PASSWORD" \
-    --data-binary "ping,host=$host,public=$PUBLIC_IP avg=$avg,loss=$loss $DATE"
+  curl -s -S -XPOST "$DB_HOST/write?db=$DB_NAME&precision=s&u=$DB_USERNAME&p=$DB_PASSWORD" --data-binary "ping,host=$host,public=$PUBLIC_IP avg=$avg,loss=$loss $DATE"
 
 done
